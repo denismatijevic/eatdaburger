1cc81fe8ccf39f8a70cde2e05258f3f83c2c5be6
denis matijevic <denisone90@gmail.com>
2020-09-15 23:22:26 -0600

    built the database, connected the database need to test and add prompts for burgers

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Biggies Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ring Of Fire', FALSE);
