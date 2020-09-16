b0a3e586bce96bfdbece23ff8f47a9e4f3155d26
denis matijevic <denisone90@gmail.com>
2020-09-15 18:20:30 -0600

    setting up the files and folders initial commit

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
+// require express & handlebars
 var express = require("express");
+var exphbs = require("express-handlebars");
+// require routes
+var routes = require("./controllers/burgersController.js");
+app.use(routes);
 
-var PORT = process.env.PORT || 8000;
+var PORT = process.env.PORT || 8080;
 
 var app = express();
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgersController.js");
-
-app.use(routes);
-
 app.listen(PORT, function() {
-  console.log("Listening on port:%s", PORT);
+  console.log("App listening on port:%s", PORT);
 });
