f0e45dfce783e563a62b13fa4077caaeea0a7cf6
denis matijevic <denisone90@gmail.com>
2020-10-21 00:25:17 -0600

    testing heroku deployment

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,9 +1,18 @@
+// help from tutor 
+// attaching handlers
 $(document).ready(function() {
     
-  $(".devour-form").on("submit", function(event) {
+  $(".devour").on("submit", function(event) {
     event.preventDefault();
+    var burger_id = $(this).children(".burger_id").val();
+    console.log(burger_id);
+    $.ajax({
+      method: "PUT",
+      url: "/burgers/" + burger_id
+    }).then(function(data) {
+  
+      location.reload();
+    });
 
-  })
+  });
 });
-
-module.exports = burger;
