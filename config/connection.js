44670689688fa8aab0f9418e2dbecc6f10378563
denis matijevic <denisone90@gmail.com>
2020-10-24 12:47:27 -0600

    Figuring out heroku deployment

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+// requiring my sql to be used
+var mysql = require("mysql");
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+   connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database:"burgers_db"
+});
+}
+
+// establishing connection
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
